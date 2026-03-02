ff7398bc2b568c05951c22a05d0057ca84476874
tanbin69 <tanbinahamed226@gmail.com>
2026-03-02 20:42:27 +0600

    card section working

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 let currentTab = "all";
-
+const tabActive = ["bg-blue-500", "text-white"];
+const tabInactive = ["bg-transparent","text-slate-700","border-state-200","text-black"]
+const allContainer = document.getElementById("all-container");
+const interviewContainer = document.getElementById("interview-container");
+const rejectedContainer = document.getElementById("rejected--container");
 function switchTab(tab){
-    console.log(tab);
     const tabs = ["all","interview","rejected"];
+    for (const t of tabs)
+    {
+        const tabName = document.getElementById("tab-" + t);
+        if(t === tab){
+            tabName.classList.remove(...tabInactive);
+            tabName.classList.add(...tabActive);
+        }
+        else
+        {
+            tabName.classList.remove(...tabActive);
+            tabName.classList.add(...tabInactive);
+        }
+    }
 }
+switchTab(currentTab)
